a35e58170328ab494cf4360d55fd7c96fab7bdaf
Yoshua Kenny Nugroho <yoshuakennynugroho@gmail.com>
2020-02-03 10:58:54 +0700

    Login and Register API done (from : https://www.codepolitan.com/api-otentikasi-menggunakan-passport-laravel-59fc1153796b9)

--- config/auth.php
+++ config/auth.php
@@ -37,14 +37,13 @@ return [
 
     'guards' => [
         'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
+           'driver' => 'session',
+           'provider' => 'users',
         ],
-
+     
         'api' => [
-            'driver' => 'token',
+            'driver' => 'passport',
             'provider' => 'users',
-            'hash' => false,
         ],
     ],
 
