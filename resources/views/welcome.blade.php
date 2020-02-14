3f41266b962aa5be9d79789ecc2a39b449510464
Yoshua Kenny Nugroho <yoshuakennynugroho@gmail.com>
2020-02-14 08:57:34 +0700

    add   <meta name="dicoding:email" content="yoshuakennynugroho@gmail.com"> in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="dicoding:email" content="yoshuakennynugroho@gmail.com">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        
         <title>Laravel</title>
 
         <!-- Fonts -->
